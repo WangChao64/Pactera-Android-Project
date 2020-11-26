d11c8623e0d1f1c178d7fb6fe7a9d8328814e150
WangChao64 <chao.wang64@pactera.com>
2020-11-26 17:46:53 +0800

    Activity的生命周期

--- app/src/main/java/com/example/android_projcet/component/LifeCycleActivity.java
+++ app/src/main/java/com/example/android_projcet/component/LifeCycleActivity.java
@@ -4,9 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 
 import com.example.android_projcet.R;
@@ -25,6 +28,16 @@ public class LifeCycleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_life_cycle);
         //被创建的时候调用
         Log.i(TAG, "onCreate called.");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {//版本判断
+            Window window = getWindow();
+            // Translucent status bar
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);//设置statusbar应用所占的屏幕扩大到全屏，但是最顶上会有背景透明的状态栏，它的文字可能会盖着你的应用的标题栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
 
         Button btn = (Button) findViewById(R.id.btn);
         btn.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/android_projcet/component/TargetActivity.java
+++ app/src/main/java/com/example/android_projcet/component/TargetActivity.java
@@ -2,7 +2,13 @@ package com.example.android_projcet.component;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import com.example.android_projcet.R;
 
@@ -12,5 +18,22 @@ public class TargetActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_target);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {//版本判断
+            Window window = getWindow();
+            // Translucent status bar
+            window.setFlags(
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
+                    WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);//设置statusbar应用所占的屏幕扩大到全屏，但是最顶上会有背景透明的状态栏，它的文字可能会盖着你的应用的标题栏
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION);
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+
+            Button btn = (Button) findViewById(R.id.btn);
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    finish();
+                }
+            });
+        }
     }
 }
